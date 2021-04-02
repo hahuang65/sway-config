7bf1e1e67c4b29a0ea1ac1afbbd7a116164bf0b0
Howard Huang <h.huang65@gmail.com>
2021-04-02 14:10:06 -0700

    Use kanshi for switching between docked and undocked monitor configurations

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
-#/bin/sh
+#!/bin/sh
 
 if [ $(uname) = "Linux" ]; then
   mkdir -p "${HOME}/.config/sway"
+  mkdir -p "${HOME}/.config/environment.d"
   ln -sf "${PWD}/config" "${HOME}/.config/sway/"
+  ln -sf "${PWD}/wlroots.conf" "${HOME}/.config/environment.d/"
 fi
