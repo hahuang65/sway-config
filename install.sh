4f5ea12362e0ab2786cdac188fc6b75e198736db
Howard Huang <hao@hwrd.me>
2023-09-09 11:59:53 -0700

    Create sway-session.target to allow Kanshi to run as a systemd unit

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 if [ $(uname) = "Linux" ]; then
   mkdir -p "${HOME}/.config/sway"
   mkdir -p "${HOME}/.config/environment.d"
+  mkdir -p "${HOME}/.config/systemd/user"
   ln -sf "${PWD}/config" "${HOME}/.config/sway/"
   ln -sf "${PWD}/wlroots.conf" "${HOME}/.config/environment.d/"
+  ln -sf "${PWD}/sway-session.target" "${HOME}/.config/systemd/user/sway-session.target"
+  sudo ln -sf "${PWD}/10-systemd" "/etc/sway/config.d/10-systemd"
 fi
